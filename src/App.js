0ba258f92140100ac90f94633e277dbf540066cd
yeah <yeah981126@gmail.com>
2025-11-26 11:05:34 +0900

    kimchi and samgiopsal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
+import React from "react";
+//import Potato from "./Potato";
+
+function Food({name, picture}){
+  return (
+    <div>
+      <h1>I love {name} </h1>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://i.namu.wiki/i/IQUUvqSoy9bFErnGmPrFT5oDwvKi9ZaKitfBOGQZwOgdpSTVJE6QkWrObvxaeYpMv1raJqMyASttO_eUykJkwRKXRjDYm01T8bTS0qr_frsbLLa4TH0v1r42nnq2WdpG7jTf4j8LrmU49E8X3SXRxg.webp',
+  },
+  {
+    name: 'Samgiopsal',
+    image: 'https://i.namu.wiki/i/oFHlYDjoEh8f-cc3lNK9jAemRkbXxNGwUg7XiW5LGS6DF1P2x8GCeNQxbQhVIwtUS1u53YPw-uoyqpmLtrGNJA.webp',
+  }
+];
+
 function App() {
-  return <div> Hello </div>
+  return (
+  <div> 
+    {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.image}/>))}
+  </div>
+  );
 }
 export default App;
